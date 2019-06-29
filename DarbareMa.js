0e31b15f325dd343a2b15cd31209916dc77f810c
pooriavakili <pooriavakili09@gmail.com>
2019-06-29 21:06:56 +0430

    first commit

--- DarbareMa.js
+++ DarbareMa.js
@@ -1,15 +1,48 @@
-import React, {Component} from 'react';
+import React, { PureComponent } from 'react';
+import { Dimensions,Text,  StyleSheet, View } from 'react-native';
 
-import {View,Text}from 'react-native'
-export default class darbareMa extends Component {
+import SwiperFlatList from 'react-native-swiper-flatlist';
+
+export default class App extends PureComponent {
     render() {
         return (
-            <View>
-                <Text>salam</Text>
+            <View style={styles.container}>
+                <SwiperFlatList
+                    autoplayDelay={2}
+                    autoplayLoop
+                    index={2}
+                    showPagination
+                >
+                    <View style={[styles.child, { backgroundColor: 'tomato' }]}>
+                        <Text style={styles.text}>همه پرداخت ها با این اپ میتوانید انجام بدهید</Text>
+                    </View>
+                    <View style={[styles.child, { backgroundColor: 'thistle' }]}>
+                        <Text style={styles.text}>بهترین دوست و بهترین همکار شما</Text>
+                    </View>
+                    <View style={[styles.child, { backgroundColor: 'skyblue' }]}>
+                        <Text style={styles.text}>خیالتان از پرداخت راحت</Text>
+                    </View>
+
+                </SwiperFlatList>
             </View>
         );
     }
 }
+export const { width, height } = Dimensions.get('window');
 
-
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    child: {
+        top:170,
+        height: height * 0.5,
+        width,
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 30,
+        textAlign: 'center'
+    }
+});
